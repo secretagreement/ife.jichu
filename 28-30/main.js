d860745bfc0f2af3d13dd31c28505e8e75b74b34
secretagreement <424557291@qq.com>
2020-03-24 00:44:46 +0800

    Update main.js
    完成了@后缀输入的功能

--- 28-30/main.js
+++ 28-30/main.js
@@ -12,28 +12,68 @@ input.oninput=function(){
 
 function getInput(){
 	var input=document.getElementById('email-input');
-	if(input.value.indexOf('@')==-1){
-		var first_email=input.value.trim();
+	var input_result=input.value.trim();
+	return input_result;
+	
+}
+
+
+function getText(){
+	var input_result=getInput();//局部变量就算return了，还是局部变量
+	var first_email;	//@前面的内容
+	var code;
+	if(input_result==''){
+		return false;
+	}else if(input_result.indexOf('@')==-1){
+		first_email=input_result;
+
 	}else{
-		first_email=input.value.substring(0,input.value.indexOf('@')).trim();
-		var second_email=
+		first_email=input_result.substring(0,input_result.indexOf('@'));
+		code=input_result.substring(input_result.indexOf('@')+1,input_result.length);
 	}
 
+
+
+
+	//@后面的内容
+	var postfixList=['163.com','gmail.com','126.com','qq.com','263.net'];
+	var second_email=[];
 	
-	return first_email;
+	for(var i=0;i<=postfixList.length-1;i++){
+		if(postfixList[i].indexOf(code)==0){//如果@后面的内容满足，则push
+			second_email.push(postfixList[i]);
+		}
+	}
+	if(second_email.length==0){
+		second_email=postfixList;//如果没有可以满足的,则每个后缀都要显示
+	}
+	
+
+
+	//连接@前面和后面的内容
+	var liText=new Array();
+	for(var j=0;j<=second_email.length-1;j++){
+		liText[j]=first_email.concat('@',second_email[j]);
+	}
+	
+
+
+	console.log(liText);
+	return liText;
 }
 
 
 function getLi(){
-	var postfixList=['163.com','gmail.com','126.com','263.net'];
-	var first_email=getInput();
+	// var postfixList=['163.com','gmail.com','126.com','263.net'];
+	var liText=getText();
 	var li=new Array();
-	var email;
-	for(var i=0;i<=postfixList.length-1;i++){
+	// var email;
+	for(var i=0;i<=liText.length-1;i++){
 		li[i]=document.createElement('li');
-		email=first_email.concat('@',postfixList[i]);
-		li[i].appendChild(document.createTextNode(email));
+		// email=first_email.concat('@',postfixList[i]);
+		li[i].appendChild(document.createTextNode(liText[i]));
 	}
+	console.log(li);
 	return li;
 
 }
@@ -41,20 +81,22 @@ function getLi(){
 
 function getUl(){
 	var li=getLi();
+	ul.innerHTML="";
 	for(var i=0;i<=li.length-1;i++){
-<<<<<<< HEAD
-		if(ul.children[i]){
+		ul.appendChild(li[i]);
 			//ul.children[i]=li[i];
-			ul.children[i].innerHTML=li[i].innerHTML;
+			//ul.children[i].innerHTML=li[i].innerHTML;
 			//为啥上面的不行，下面的可以
-		}else{
-			ul.appendChild(li[i]);
-		}
-=======
+
+
+			
+
 		//ul.appendChild(li[i]);
-		ul.elementChild[i]=li[i];
+		//ul.elementChild[i]=li[i];
 		// ul.children[i]=li[i];
->>>>>>> 603cc493c0fab4af0e15bc5544d1d454e8d69dd5
+
+
+
 	}
 }
 
